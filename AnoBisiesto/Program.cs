db89ac47ffa963b91c3a4eaf5d3e0a86a53a9c4f
Kevin Montaño <Kevin Montao@DESKTOP-IKA0HN7>
2024-03-07 23:17:25 -0500

    ano bisiesto

---
+++ AnoBisiesto/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresar año ");
+        double ano = Convert.ToDouble(Console.ReadLine());
+
+        if (ano % 4 != 0)
+        {
+            Console.WriteLine($"No es bisiesto {ano}");
+        }
+        else if (ano % 100 != 0) 
+        {
+            Console.WriteLine($"Es bisiesto {ano}");
+        }
+        else if (ano % 400!= 0) 
+        {
+            Console.WriteLine($"No es bisiesto {ano}");
+        }
+    }
+}
